9cc5fb7afb10e65ba468ff419202f0fe8cba3eb4
AKZ <75739105+darkelfs56@users.noreply.github.com>
2025-01-15 23:35:31 +0800

    Add comments about storing and using github token with AWS.

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -23,10 +23,20 @@ export class CiCdAwsPipelineDemoStack extends cdk.Stack {
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub(
           "darkelfs56/ci-cd-aws-pipeline-demo",
-          "main"
+          "main",
+          {
+            authentication: cdk.SecretValue.secretsManager(
+              "ci-cd-aws-pipeline-demo-github-token"
+            ),
+          }
         ),
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
     });
   }
 }
+
+//Need to create Github Access Token, as secret for AWS services
+//1. Go to secrets-manager
+//2. Store as other secret
+//3. Store as plaintext; Just copy paste the whole access token string
