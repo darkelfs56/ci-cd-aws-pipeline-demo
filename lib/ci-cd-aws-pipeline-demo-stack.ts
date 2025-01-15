87a1449112b8a942255b4c51bda4f503a450a702
AKZ <75739105+darkelfs56@users.noreply.github.com>
2025-01-15 23:05:21 +0800

    Add initial code for creating CodePipeline.

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -1,5 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CiCdAwsPipelineDemoStack extends cdk.Stack {
@@ -12,5 +17,16 @@ export class CiCdAwsPipelineDemoStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'CiCdAwsPipelineDemoQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    new CodePipeline(this, "Pipeline", {
+      pipelineName: "TestPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub(
+          "darkelfs56/ci-cd-aws-pipeline-demo",
+          "main"
+        ),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
   }
 }
